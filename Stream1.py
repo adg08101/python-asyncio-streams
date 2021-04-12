f681724720a965159878f76fb91eaa2b970a7228
klol <adg08101@gmail.com>
2021-04-12 09:28:43 -0400

    Port adjust
    Adjust port to write

--- Stream1.py
+++ Stream1.py
@@ -1,19 +1,28 @@
 import asyncio
 
-async def tcp_echo_client(message):
-    reader, writer = await asyncio.open_connection(
-        '127.0.0.1', 8080)
+async def handle_echo(reader, writer):
+    data = await reader.read(100)
+    message = data.decode()
+    addr = writer.get_extra_info('peername')
 
-    print(f'Send: {message!r}')
-    writer.write(message.encode())
+    print(f"Received {message!r} from {addr!r}")
+
+    print(f"Send: {message!r}")
+    writer.write(data)
     await writer.drain()
 
-    data = await reader.read(50)
-    print(data)
-
-    print('Close the connection')
+    print("Close the connection")
     writer.close()
-    await writer.wait_closed()
+
+async def main():
+    server = await asyncio.start_server(
+        handle_echo, '127.0.0.1', 80)
+
+    addr = server.sockets[0].getsockname()
+    print(f'Serving on {addr}')
+
+    async with server:
+        await server.serve_forever()
 
 if __name__ == '__main__':
-    asyncio.run(tcp_echo_client('HelloWorld, from Cuba!'))
+    asyncio.run(main())
