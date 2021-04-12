ffeac18935153faa7d71980217a011b455759ab0
klol <adg08101@gmail.com>
2021-04-12 09:27:22 -0400

    Streams w Server
    Add server instance

--- Stream1.py
+++ Stream1.py
@@ -2,7 +2,7 @@ import asyncio
 
 async def tcp_echo_client(message):
     reader, writer = await asyncio.open_connection(
-        '127.0.0.1', 80)
+        '127.0.0.1', 8080)
 
     print(f'Send: {message!r}')
     writer.write(message.encode())
